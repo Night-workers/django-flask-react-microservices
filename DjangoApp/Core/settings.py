31c8a089053c12b31b5692256e8505a461fc0db2
iinfotech <infotech@imperotechne.com>
2021-12-05 18:01:37 +0100

    updated django app

--- DjangoApp/Core/settings.py
+++ DjangoApp/Core/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'db-django-service',
-        'PORT': '33066',
+        'PORT': '3306',
         # 'HOST': '127.0.0.1',
         # 'PORT': '33066',
     }
